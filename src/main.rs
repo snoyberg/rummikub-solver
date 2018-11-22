cc4d3bfda515451a23ecf27bbf7409158047edfb
Michael Snoyman <michael@snoyman.com>
2018-11-22 08:09:11 +0200

    Prettier tiles

--- src/main.rs
+++ src/main.rs
@@ -33,24 +33,44 @@ fn build() -> Result<(), Box<std::error::Error>> {
 
     let style = doc.create_element("style")?;
     let css = r#"
-        td.rank {
-            text-align: right;
-            margin-left: 3px;
-        }
-/*
-        table {
-            border-collapse: collapse;
-        }
-        td {
-            border: 1px solid black;
-            padding: 5px;
-            font-size: 0.4em;
-        }
-        div.rank {
-            font-size: 2em;
-            font-weight: bold;
-        }
-*/
+td.rank {
+    text-align: right;
+    margin-left: 3px;
+}
+.tile {
+    display: inline-block;
+    border: 1px solid #999;
+    background-color: #eee;
+    padding: 0 3px;
+    width: 1.5em;
+    text-align: center;
+}
+.tile.red {
+    color: red;
+}
+.tile.orange {
+    color: orange;
+}
+.tile.blue {
+    color: blue;
+}
+.combo {
+    margin-right: 1em;
+}
+ul.solutions {
+    list-style: none;
+    padding: 0;
+}
+ul.solutions > li:first-child {
+    border-top: 0;
+    margin-top: 0;
+    padding-top: 0;
+}
+ul.solutions > li {
+    margin-top: 0.5em;
+    padding-top: 0.5em;
+    border-top: 1px solid black;
+}
 "#;
     style.append_child(&doc.create_text_node(css));
     head.append_child(&style);
@@ -76,18 +96,30 @@ fn build() -> Result<(), Box<std::error::Error>> {
     sol_column.set_attribute("class", "col-lg-6")?;
     row.append_child(&sol_column);
 
-    let solution = doc.create_element("pre")?;
-    sol_column.append_child(&solution);
-    let solution = Rc::new(solution);
+    let solutions_h1 = doc.create_element("h1")?;
+    solutions_h1.set_attribute("class", "text-center")?;
+    solutions_h1.append_child(&doc.create_text_node("Solutions"));
+    sol_column.append_child(&solutions_h1);
+
+    let solution_ul = doc.create_element("ul")?;
+    solution_ul.set_attribute("class", "solutions")?;
+    sol_column.append_child(&solution_ul);
+    let solution_ul = Rc::new(solution_ul);
+
+    let board_h1 = doc.create_element("h1")?;
+    board_h1.set_attribute("class", "text-center")?;
+    board_column.append_child(&board_h1);
+    board_h1.append_child(&doc.create_text_node("Board"));
 
     let table = doc.create_element("table")?;
+    board_column.append_child(&table);
 
     let tbody = doc.create_element("tbody")?;
     table.append_child(&tbody);
 
     let tiles = Rc::new(RefCell::new(Tiles::new()));
 
-    let mut make_tile: impl FnMut(&Document, &Element, Tile) -> Result<(), InvalidCharacterError> = move |doc: &Document, td: &Element, tile: Tile| {
+    let mut make_buttons: impl FnMut(&Document, &Element, Tile) -> Result<(), InvalidCharacterError> = move |doc: &Document, td: &Element, tile: Tile| {
         let button_group = doc.create_element("div")?;
         button_group.set_attribute("class", "btn-group")?;
         td.append_child(&button_group);
@@ -108,28 +140,49 @@ fn build() -> Result<(), Box<std::error::Error>> {
 
             let buttons = buttons.clone();
             let tiles = tiles.clone();
-            let solution = solution.clone();
-            button.add_event_listener(move |_: ClickEvent| {
+            let solution_ul = solution_ul.clone();
+            let callback = move || {
+                let doc = document();
                 for (button, innercount) in buttons.iter().zip(0..) {
                     let class = if count == innercount {ACTIVE} else {INACTIVE};
-                    match button.set_attribute("class", class) {
-                        Ok(()) => (),
-                        Err(e) => {
-                            window().alert(&format!("Well that's weird: {}", e));
-                        }
-                    }
+                    button.set_attribute("class", class)?;
+                }
+
+                for node in solution_ul.child_nodes() {
+                    solution_ul.remove_child(&node)?;
                 }
 
                 let mut tiles = tiles.borrow_mut();
                 tiles.set_count(&tile, count);
                 let solutions = solve(*tiles);
-                let mut solutions_text = String::from("Valid solutions:\n");
-                for sol in solutions {
-                    solutions_text += "Solution: ";
-                    solutions_text += &sol.to_string();
-                    solutions_text += "\n";
+
+                for sol in solutions.into_iter().rev().filter(|sol| sol.leftover_jokers == 0) {
+                    let li = doc.create_element("li")?;
+                    solution_ul.append_child(&li);
+
+                    for combo in sol.combos {
+                        let span = doc.create_element("span")?;
+                        span.set_attribute("class", "combo")?;
+                        li.append_child(&span);
+
+                        for tile in Tile::all() {
+                            let count = combo.get_count(&tile);
+                            assert!(count < 2);
+                            if count == 1 {
+                                span.append_child(&tile_span(&doc, &tile)?);
+                            }
+                        }
+                    }
                 }
-                solution.set_text_content(&solutions_text);
+
+                let res: Result<(), Box<std::error::Error>> = Ok(());
+                res
+            };
+            button.add_event_listener(move |_: ClickEvent| {
+                match callback() {
+                    Ok(()) => (),
+                    Err(e) => window().alert(&format!("That was weird: {}", e)),
+                }
             });
         }
 
@@ -142,18 +195,13 @@ fn build() -> Result<(), Box<std::error::Error>> {
 
         for color in Color::all() {
             let td = doc.create_element("td")?;
+            let tile = Tile::Number(rank, color);
+            td.append_child(&tile_span(&doc, &tile)?);
             td.set_attribute("class", "rank")?;
-            td.set_attribute("style", &format!("color: {}", match color {
-                Color::Black => "black",
-                Color::Blue => "blue",
-                Color::Orange => "orange",
-                Color::Red => "red",
-            }))?;
-            td.append_child(&doc.create_text_node(&rank.to_string()));
             row.append_child(&td);
 
             let td = doc.create_element("td")?;
-            make_tile(&doc, &td, Tile::Number(rank, color))?;
+            make_buttons(&doc, &td, tile)?;
             row.append_child(&td);
 
         }
@@ -163,15 +211,34 @@ fn build() -> Result<(), Box<std::error::Error>> {
     tbody.append_child(&row);
 
     let td = doc.create_element("td")?;
-    td.append_child(&doc.create_text_node("Joker"));
+    td.append_child(&tile_span(&doc, &Tile::Joker)?);
     row.append_child(&td);
 
     let td = doc.create_element("td")?;
     td.set_attribute("colspan", "7")?;
     row.append_child(&td);
-    make_tile(&doc, &td, Tile::Joker)?;
-
-    board_column.append_child(&table);
+    make_buttons(&doc, &td, Tile::Joker)?;
 
     Ok(())
+}
+
+fn tile_span(doc: &Document, tile: &Tile) -> Result<Element, InvalidCharacterError> {
+    let span = doc.create_element("span")?;
+    match tile {
+        Tile::Joker => {
+            span.set_attribute("class", "tile")?;
+            span.append_child(&doc.create_text_node("J"));
+        }
+        Tile::Number(rank, color) => {
+            let class = match color {
+                Color::Black => "tile black",
+                Color::Blue => "tile blue",
+                Color::Orange => "tile orange",
+                Color::Red => "tile red",
+            };
+            span.set_attribute("class", class)?;
+            span.append_child(&doc.create_text_node(&rank.to_string()));
+        }
+    }
+    Ok(span)
 }
